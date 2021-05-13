8ee51e86fece0b463c3f92d7ec7acfbc7aa1bf34
Poppy Tait <poppytait@gmail.com>
2021-05-13 17:17:17 +0200

    Inject YouTube into YouTubeService via constructor

--- src/main/java/com/poppytait/cyclingvideosapi/config/Config.java
+++ src/main/java/com/poppytait/cyclingvideosapi/config/Config.java
@@ -1,11 +1,18 @@
 package com.poppytait.cyclingvideosapi.config;
 
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.youtube.YouTube;
 import com.poppytait.cyclingvideosapi.util.ResourceReader;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -28,6 +35,18 @@ public class Config {
         String asString = ResourceReader.asString(searchTermsFile);
         List<String> searchTerms = asString.lines().map(term -> "\"" + term + "\"").collect(Collectors.toList());
         return new YouTubeConfig(apiKey, gcnChannelId, mbnChannelId, searchTerms);
+    }
+
+    @Bean
+    public YouTube youTube() throws GeneralSecurityException, IOException {
+        String APPLICATION_NAME = "Cycling Videos API";
+        JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+
+        final NetHttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+
+        return new YouTube.Builder(httpTransport, JSON_FACTORY, null)
+                .setApplicationName(APPLICATION_NAME)
+                .build();
     }
 
     public static class YouTubeConfig {
--- src/main/java/com/poppytait/cyclingvideosapi/service/YouTubeService.java
+++ src/main/java/com/poppytait/cyclingvideosapi/service/YouTubeService.java
@@ -1,9 +1,5 @@
 package com.poppytait.cyclingvideosapi.service;
 
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.youtube.YouTube;
 import com.google.api.services.youtube.model.SearchListResponse;
 import com.google.api.services.youtube.model.SearchResult;
@@ -11,14 +7,11 @@ import com.poppytait.cyclingvideosapi.config.Config;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-import java.security.GeneralSecurityException;
 import java.util.Collections;
 import java.util.List;
 
 @Service
 public class YouTubeService implements IYouTubeService {
-    private static final String APPLICATION_NAME = "Cycling Videos API";
-    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
     private static final String PART = "snippet";
     private static final String TYPE = "video";
     private static final long MAX_RESULTS = 100L;
@@ -27,13 +20,10 @@ public class YouTubeService implements IYouTubeService {
     private final Config.YouTubeConfig config;
     private final String searchTerms;
 
-    public YouTubeService(Config.YouTubeConfig config) throws GeneralSecurityException, IOException {
+    public YouTubeService(Config.YouTubeConfig config, YouTube youTube) {
         this.config = config;
         this.searchTerms = String.join("|", config.getSearchTerms());
-        final NetHttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
-        this.youTube = new YouTube.Builder(httpTransport, JSON_FACTORY, null)
-                .setApplicationName(APPLICATION_NAME)
-                .build();
+        this.youTube = youTube;
     }
 
     @Override
